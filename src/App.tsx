6cfb8a41ac85698d564f7a4671e0fa70f77933e0
mozieblo <m.ozieblo@wp.pl>
2021-04-24 23:22:15 +0200

    Styled Component with TypeScript - example

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React, { FC } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { ROUTES } from './router/router';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
+import { globalTheme } from './styles/theme';
 
 const App: FC = () => {
     const { HOME, ABOUT, CONTACT } = ROUTES;
 
     return (
-        <Switch>
-            <Route path={HOME} exact component={Home} />
-            <Route path={ABOUT} exact component={About} />
-            <Route path={CONTACT} exact component={Contact} />
-            <Redirect to={HOME} />
-        </Switch>
+        <ThemeProvider theme={globalTheme}>
+            <Switch>
+                <Route path={HOME} exact component={Home} />
+                <Route path={ABOUT} exact component={About} />
+                <Route path={CONTACT} exact component={Contact} />
+                <Redirect to={HOME} />
+            </Switch>
+        </ThemeProvider>
     );
 };
 
